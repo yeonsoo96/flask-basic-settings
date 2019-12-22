73542f4e5f2f7056cf61a4ebd85cfc70ecca05f8
yeonsooKim <gjb2628@gmail.com>
2019-12-22 22:52:00 +0900

    render example

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 from werkzeug.utils import redirect
-
+import login
 
 # define
 HOST_ADDR = '127.0.0.1'
-PORT_NUM = '8080'
+PORT_NUM = '8000'
 DEBUG = False
 
 app = Flask(__name__)
@@ -16,11 +16,14 @@ else:
     raise Exception('MODE error')
 
 @app.route("/", methods=['GET', 'POST'])
+def call_login():
+    return login.login()
 def hello():
-    response = request.form['test']  # get body
-    response += request.headers.get('test')  # get header
-    # return redirect('user/yeonsookim')
-    return response
+    # response = request.form['test']  # get body
+    # response += request.headers.get('test')  # get header
+    return redirect('login')
+    # return response
+    # return render_template('wink.html')
 
 @app.route('/user/<string:username>')
 @app.route('/name/<string:username>')
