6924fdafd19389a2180520266060875764873ff7
yeonsooKim <gjb2628@gmail.com>
2019-12-23 00:12:31 +0900

    test db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, render_template
 from werkzeug.utils import redirect
 import login
+from flask_sqlalchemy import SQLAlchemy
 
 # define
 HOST_ADDR = '127.0.0.1'
@@ -9,6 +10,9 @@ PORT_NUM = '8000'
 DEBUG = False
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
 if os.environ.get('MODE') == 'DEV':
     HOST_ADDR = '0.0.0.0'
     DEBUG = True
@@ -28,8 +32,24 @@ def hello():
 @app.route('/user/<string:username>')
 @app.route('/name/<string:username>')
 def profile(username):
+    db.create_all()
+    user = User(username+'@gmail.com')
+    db.session.add(user)
+    db.session.commit()
     return '{}\'s profile'.format(username)
 
+@app.route('/user/')
+def user():
+    user = User.query.all()
+    return str(user)
+class User(db.Model):
+    __table_name__ = 'user'
+    email = db.Column(db.String(50), primary_key=True, unique=True)
+
+    def __init__(self, email):
+        self.email = email
+    def info(self):
+        return self.email
 if __name__ == '__main__':
 
     app.run(host=HOST_ADDR, port=PORT_NUM, debug=DEBUG)
