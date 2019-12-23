d838b77d9c916d48902483fdf3c33876144d9721
yeonsookim <gjb2628@gmail.com>
2019-12-23 11:56:26 +0900

    connect postgresql

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from werkzeug.utils import redirect
 import login
 from flask_sqlalchemy import SQLAlchemy
 
+
 # define
 HOST_ADDR = '127.0.0.1'
 PORT_NUM = '8000'
@@ -13,21 +14,27 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 
 
+
+
 if os.environ.get('MODE') == 'DEV':
     HOST_ADDR = '0.0.0.0'
     DEBUG = True
 else:
     raise Exception('MODE error')
 
+
 @app.route("/", methods=['GET', 'POST'])
 def call_login():
     return login.login()
+
+
 def hello():
     # response = request.form['test']  # get body
     # response += request.headers.get('test')  # get header
     return redirect('login')
     # return response
     # return render_template('wink.html')
+
 
 @app.route('/user/<string:username>')
 @app.route('/name/<string:username>')
@@ -38,18 +45,23 @@ def profile(username):
     db.session.commit()
     return '{}\'s profile'.format(username)
 
+
 @app.route('/user/')
 def user():
     user = User.query.all()
     return str(user)
+
+
 class User(db.Model):
     __table_name__ = 'user'
     email = db.Column(db.String(50), primary_key=True, unique=True)
 
     def __init__(self, email):
         self.email = email
+
     def info(self):
         return self.email
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
     app.run(host=HOST_ADDR, port=PORT_NUM, debug=DEBUG)
