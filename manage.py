99e5b6ac05ed16cb70fb2ba6be46e9b0e128182b
yeonsooKim <gjb2628@gmail.com>
2020-03-05 01:46:27 +0900

    불필요 코드 제거

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
-from settings.settings import app, HOST_ADDR, SERVER_PORT, DEBUG, MODE
-from waitress import serve
 from paste.translogger import TransLogger
+from waitress import serve
 
+from settings.settings import app, HOST_ADDR, SERVER_PORT, MODE
 
 if __name__ == '__main__':
     if MODE == 'DEV':
-        app.run(host=HOST_ADDR, port=SERVER_PORT, debug=DEBUG)
+        app.run(host=HOST_ADDR, port=SERVER_PORT)
     elif MODE == 'RUN':
         serve(TransLogger(app, setup_console_handler=True),
               host=HOST_ADDR, port=SERVER_PORT)
