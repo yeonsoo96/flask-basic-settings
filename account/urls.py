144c37bb8515fc35084b40718eff5ce6135e6c5c
yeonsooKim <gjb2628@gmail.com>
2020-03-05 02:41:15 +0900

    delete

--- account/urls.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from .views import AccountUser
-app = Blueprint('account', __name__)
-
-login_view = AccountUser.as_view('account_user_view')
-app.add_url_rule('/user/', view_func=login_view)
