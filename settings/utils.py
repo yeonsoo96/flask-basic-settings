103b620c9fd55e017bc04ef0a3c0de2f565c2475
yeonsooKim <gjb2628@gmail.com>
2020-03-05 02:43:57 +0900

    구조 변경에 맞추어 api decorator 함수형으로 변경

--- settings/utils.py
+++ settings/utils.py
@@ -4,12 +4,12 @@ from flask import request
 
 def api(f):
     @functools.wraps(f)
-    def deco(self, *args, **kwargs):
+    def deco(*args, **kwargs):
         if request.method in ['GET', 'DELETE']:
             data = request.args.to_dict()
         elif request.method in ['POST', 'PUT']:
             data = request.form
         else:
             data = {}
-        return f(self, data, *args, **kwargs)
+        return f(data, *args, **kwargs)
     return deco
