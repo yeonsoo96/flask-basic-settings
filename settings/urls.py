a8e950c0ece9f249e55a9f9db41892dac931a8b5
yeonsooKim <gjb2628@gmail.com>
2020-03-04 23:48:13 +0900

    git hooks url 추가

--- settings/urls.py
+++ settings/urls.py
@@ -7,11 +7,24 @@
 from settings.settings import app
 from login import urls as loginurls
 from account import urls as accounturls
+from flask import json, request, Response
+import os
 
 
 @app.route('/ping', methods=['GET'])
 def ping():  # 서버 연결 테스트용
     return 'ping'
+
+
+@app.route('/deploy', methods=['POST'])
+def hooks():
+    res = json.dumps(request.form)
+    res = json.loads(res)
+    res = json.loads(res['payload'])
+    root_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    if res['ref'] == 'refs/heads/deploy':
+        os.system(f'sh {root_dir}/settings/hooks.sh')
+    return Response('push', status=200)
 
 
 app.register_blueprint(loginurls.app, url_prefix='/login')  # /login으로 시작하면 login_app으로 연결
