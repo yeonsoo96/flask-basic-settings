f33e1443f5d2ad31a31d211d7da39a37691e8085
yeonsooKim <gjb2628@gmail.com>
2020-03-04 23:33:32 +0900

    CORS and pytest 인식코드 추가

--- settings/settings.py
+++ settings/settings.py
@@ -1,48 +1,47 @@
 import os
+import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
-
+from flask_cors import CORS
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 # define
 HOST_ADDR = '127.0.0.1'  # localhost 주소
-PORT_NUM = '8000'  # 서버 포트
+SERVER_PORT = '8000'  # 서버 포트
 DEBUG = False  # 디버그모드
-
+MODE = os.environ.get('MODE')
 
 # connect db
 USER = 'postgres'  # username
 PASSWOLRD = 'root'  # postgresql pw
-PORT = '5432'  # postgresql port
+DB_PORT = '5432'  # postgresql port
 NAME = 'test'  # db name
-POSTGRESQL = f'postgresql://{USER}:{PASSWOLRD}@{HOST_ADDR}:{PORT}/{NAME}'  # postgresql uri
+POSTGRESQL = f'postgresql://{USER}:{PASSWOLRD}@{HOST_ADDR}:{DB_PORT}/{NAME}'  # postgresql uri
 
 
 # app settings
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRESQL  # db connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+CORS(app)
 
 # select operation mode
-if os.environ.get('MODE') == 'DEV':  # mode - development
+if MODE == 'TEST' or sys.argv[0].endswith('test'):  # use only pytest
+    HOST_ADDR = '127.0.0.1'
+    DEBUG = False
+elif MODE == 'DEV':  # mode - development
     HOST_ADDR = '127.0.0.1'
     DEBUG = True
-elif os.environ.get('MODE') == 'RUN':  # mode - release
+elif MODE == 'RUN':  # mode - release
     HOST_ADDR = '0.0.0.0'
     DEBUG = False
 else:  # select not permission mode
-    raise Exception('MODE error')
+    raise KeyError
 
 
 # manager
 app.debug = DEBUG  # 실행 모드에 따라 디버그 온오프
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-manager.add_command('runserver', Server(host=HOST_ADDR, port=PORT_NUM))
 
 
 # connect models
