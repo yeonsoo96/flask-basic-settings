917f81e6014464ee7897dba09df6618ce3fad935
yeonsooKim <gjb2628@gmail.com>
2020-03-04 23:31:10 +0900

    wsgi 추가

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@
 오로지 실행파일.
 모든 설정은 세팅 폴더 안에서 관리 됨.
 '''
-from settings.settings import manager
+from settings.settings import app, HOST_ADDR, SERVER_PORT, DEBUG, MODE
+from waitress import serve
+from paste.translogger import TransLogger
+
 
 if __name__ == '__main__':
-    manager.run()
+    if MODE == 'DEV':
+        app.run(host=HOST_ADDR, port=SERVER_PORT, debug=DEBUG)
+    elif MODE == 'RUN':
+        serve(TransLogger(app, setup_console_handler=True),
+              host=HOST_ADDR, port=SERVER_PORT)
