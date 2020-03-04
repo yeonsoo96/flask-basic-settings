ec6a0abf1ca35ebeff47c95d59e5e520ba42ff50
yeonsooKim <gjb2628@gmail.com>
2020-03-05 00:18:05 +0900

    delete

--- login/urls.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from .views import Login
-
-app = Blueprint('login', __name__)
-
-login_view = Login.as_view('login_view')
-app.add_url_rule('/', view_func=login_view)
-
--- login/views.py
+++
@@ -1,8 +0,0 @@
-from flask.views import MethodView
-
-class Login(MethodView):
-    def get(self):
-        return 'get'
-
-    def post(self):
-        return 'post'
