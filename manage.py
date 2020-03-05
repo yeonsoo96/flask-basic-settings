27fa45988f7a121f235c3854af65ffb4eeead435
yeonsooKim <gjb2628@gmail.com>
2020-03-06 03:07:07 +0900

    wsgi 모듈 변경

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
-from paste.translogger import TransLogger
-from waitress import serve
+from gevent.pywsgi import WSGIServer
 
 from settings.settings import app, HOST_ADDR, SERVER_PORT, MODE
 
@@ -7,5 +6,5 @@ if __name__ == '__main__':
     if MODE == 'DEV':
         app.run(host=HOST_ADDR, port=SERVER_PORT)
     elif MODE == 'RUN':
-        serve(TransLogger(app, setup_console_handler=True),
-              host=HOST_ADDR, port=SERVER_PORT)
+        app = WSGIServer((HOST_ADDR, SERVER_PORT), app)
+        app.serve_forever()
