79a99bc9d42659252f345833e60bfb0ed8bdfa2a
yeonsooKim <gjb2628@gmail.com>
2020-03-07 04:11:52 +0900

    db settings에서 가져옴, gevent log 끔

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
 from gevent.pywsgi import WSGIServer
 
-from settings.settings import app, HOST_ADDR, SERVER_PORT, MODE
+from settings.settings import HOST_ADDR, SERVER_PORT, MODE
+from settings.wsgi import app
 
+# manager
+db = SQLAlchemy(app)  # connect db
 if __name__ == '__main__':
     if MODE == 'DEV':
         app.run(host=HOST_ADDR, port=SERVER_PORT)
     elif MODE == 'RUN':
-        app = WSGIServer((HOST_ADDR, SERVER_PORT), app)
+        app = WSGIServer((HOST_ADDR, SERVER_PORT), app, log=None)
         app.serve_forever()
