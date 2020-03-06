5156fa8cc9db1780b1f23e6c58df6141bdd8d631
yeonsooKim <gjb2628@gmail.com>
2020-03-07 04:27:50 +0900

    앱 생성과 로그 처리를 여기서 하도록 수정

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
+import time
+
+from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from gevent.pywsgi import WSGIServer
 
+from settings.logger import logger
 from settings.settings import HOST_ADDR, SERVER_PORT, MODE
-from settings.wsgi import app
+from settings.wsgi import create_wsgi
+
+app = create_wsgi()
 
 # manager
 db = SQLAlchemy(app)  # connect db
@@ -12,3 +18,10 @@ if __name__ == '__main__':
     elif MODE == 'RUN':
         app = WSGIServer((HOST_ADDR, SERVER_PORT), app, log=None)
         app.serve_forever()
+
+
+@app.after_request
+def after_request(response):  # 리스폰스를 반환하면서 로그를 남김
+    logger.info(f'{request.remote_addr} {time.strftime("%Y-%m-%d  %X", time.localtime(time.time()))}  '
+                f'{request.method} {request.url} {response.status_code} - {request.user_agent}')
+    return response
