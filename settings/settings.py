a20bf1dc98a52b6423b61b2a36415e03550bed24
yeonsooKim <gjb2628@gmail.com>
2020-03-07 04:09:37 +0900

    app -> wsgi.py로 이동

--- settings/settings.py
+++ settings/settings.py
@@ -1,13 +1,5 @@
-import logging
 import os
 import sys
-
-from flask import Flask, request
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-
-from api.server import app as api_server
-from api.test import app as api_test
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
@@ -24,7 +16,6 @@ DB_PORT = '5432'  # postgresql port
 NAME = 'test'  # db name
 POSTGRESQL = f'postgresql://{USER}:{PASSWOLRD}@{HOST_ADDR}:{DB_PORT}/{NAME}'  # postgresql uri
 
-
 # select operation mode
 if MODE == 'TEST' or sys.argv[0].endswith('test'):  # use only pytest
     HOST_ADDR = '127.0.0.1'
@@ -37,33 +28,3 @@ elif MODE == 'RUN':  # mode - release
     DEBUG = False
 else:  # select not permission mode
     raise KeyError
-
-
-# app settings
-app = Flask(__name__)
-app.debug = DEBUG  # debug mode
-app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRESQL  # db connect
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['LOGGING_LEVEL'] = logging.DEBUG
-CORS(app)
-
-
-@app.before_request
-def before_request():
-    pass
-
-@app.after_request
-def after_request(response):
-    print(request.environ.get('HTTP_X_REAL_IP', request.remote_addr), request.method, request.url, response.status_code)
-    return response
-
-
-# app connections
-app.register_blueprint(api_server)
-app.register_blueprint(api_test)
-
-
-# manager
-db = SQLAlchemy(app)
-
-
