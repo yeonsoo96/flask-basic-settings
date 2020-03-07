1bb9cbc6ab4f077b9f5a606c2036cbf8385e9654
yeonsooKim <gjb2628@gmail.com>
2020-03-08 02:42:05 +0900

    로그 관련 코드들 여기로 위치 이동

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -1,9 +1,33 @@
+import time
+import traceback
+
 from flask import Flask
+from flask import request
 from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
 
 from api.server import app as api_server
 from api.test import app as api_test
+from settings.logger import create_logger
 from settings.settings import DEBUG, POSTGRESQL
+
+logger = create_logger()
+
+
+def after_request(response):  # 정상적으로 처리시 로그를 남김
+    logger.info(f'{request.remote_addr} {time.strftime("%Y-%m-%d  %X", time.localtime(time.time()))}  '
+                f'{request.method} {request.url} {response.status_code} - {request.user_agent}')
+    return response
+
+
+def trace_back_recent_call():  # 오류가 난 코드의 위치를 스트링으로 반환함.
+    error_location = traceback.format_exc().split('\n')[15]
+    return error_location
+
+
+def error_handler(error):  # 에러 발생시 로그를 남김
+    logger.error(f'{request.remote_addr} {time.strftime("%Y-%m-%d  %X", time.localtime(time.time()))} '
+                 f'{request.method} {request.url} {error.code} - {request.user_agent} \n {trace_back_recent_call()}')
 
 
 def create_wsgi():
@@ -12,6 +36,8 @@ def create_wsgi():
     app.debug = DEBUG  # debug mode
     app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRESQL  # db connect
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.after_request(after_request)
+    app.register_error_handler(HTTPException, error_handler)
     CORS(app)
 
     # app connections
