041da891f5ae26c81936ebf4ef7359d8bb32855b
yeonsooKim <gjb2628@gmail.com>
2020-03-08 02:42:29 +0900

    로그 관련 코드들 wsgi.py로 이동

--- manage.py
+++ manage.py
@@ -1,23 +1,10 @@
-import time
-
-from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from gevent.pywsgi import WSGIServer
 
-from settings.logger import logger
 from settings.settings import HOST_ADDR, SERVER_PORT, MODE
 from settings.wsgi import create_wsgi
 
 app = create_wsgi()
-
-
-# todo 서버에서 로그가 안남는거 해결하기
-@app.after_request
-def after_request(response):  # 리스폰스를 반환하면서 로그를 남김
-    logger.info(f'{request.remote_addr} {time.strftime("%Y-%m-%d  %X", time.localtime(time.time()))}  '
-                f'{request.method} {request.url} {response.status_code} - {request.user_agent}')
-    return response
-
 
 # manager
 db = SQLAlchemy(app)  # connect db
