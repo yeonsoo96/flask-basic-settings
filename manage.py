78c149f8effed22923b332df6d458a75450aef7b
yeonsooKim <gjb2628@gmail.com>
2020-03-07 18:19:06 +0900

    로그가 안나오던 버그 수정

--- manage.py
+++ manage.py
@@ -10,6 +10,15 @@ from settings.wsgi import create_wsgi
 
 app = create_wsgi()
 
+
+# todo 서버에서 로그가 안남는거 해결하기
+@app.after_request
+def after_request(response):  # 리스폰스를 반환하면서 로그를 남김
+    logger.info(f'{request.remote_addr} {time.strftime("%Y-%m-%d  %X", time.localtime(time.time()))}  '
+                f'{request.method} {request.url} {response.status_code} - {request.user_agent}')
+    return response
+
+
 # manager
 db = SQLAlchemy(app)  # connect db
 if __name__ == '__main__':
@@ -18,10 +27,3 @@ if __name__ == '__main__':
     elif MODE == 'RUN':
         app = WSGIServer((HOST_ADDR, SERVER_PORT), app, log=None)
         app.serve_forever()
-
-
-@app.after_request
-def after_request(response):  # 리스폰스를 반환하면서 로그를 남김
-    logger.info(f'{request.remote_addr} {time.strftime("%Y-%m-%d  %X", time.localtime(time.time()))}  '
-                f'{request.method} {request.url} {response.status_code} - {request.user_agent}')
-    return response
