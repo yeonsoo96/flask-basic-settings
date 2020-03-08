793ceab22ddf3a96de456854e7aa24d80276a865
yeonsooKim <gjb2628@gmail.com>
2020-03-09 02:24:02 +0900

    오타 변경

--- settings/hooks.sh
+++ settings/hooks.sh
@@ -3,6 +3,6 @@ echo "push"
 git checkout -t origin/deploy
 git pull origin deploy
 export MODE=RUN
-pip3 install -r requirement.txt
+pip3 install -r requirements.txt
 fuser -k -n tcp 8000
 python3 manage.py runserver
